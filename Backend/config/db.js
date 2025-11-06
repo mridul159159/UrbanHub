e9d7826e9527c697ba6f8405dfad41d69b4e1a67
Mridul Anand <mridulanand159159@gmail.com>
2025-11-06 11:21:47 +0530

    Modify database which local to atlas setup

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 export const dbConnect=async()=>{
-   const conn= await  mongoose.connect('mongodb://localhost:27017/Quirex');
+   const conn= await  mongoose.connect(process.env.MONGO_URI);
    if(conn){
     console.log("Db connected successfully............"); 
    }
--- Backend/index.js
+++ Backend/index.js
@@ -4,11 +4,13 @@ import router from './route/userRoute.js';
 import adminRoute from './route/adminRoute.js'
 import fileUpload from 'express-fileupload';
 import cors from 'cors'
+import dotenv from 'dotenv';
+dotenv.config();
 const app=express();
 app.use(express.json());
 app.use(fileUpload());
 app.use(cors());
-const PORT=9000; 
+const PORT = process.env.PORT || 9000;
 dbConnect();
 app.use('/img',express.static('uploads'));
  app.use('/api',router);
